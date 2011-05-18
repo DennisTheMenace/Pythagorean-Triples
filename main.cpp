728f98dbdc60fd1f5f1e3a84cfd98cbdcd386c55
Joshua Rowe <joshua@wired.com.au>
2011-05-18 21:35:48 +1000

    added a c++ file which inspired me

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+int a, b, c, i = 0, limit = 500;
+puts("Pythagorean Triples:");
+for ( a = 1; a < limit; ++a )
+{
+for ( b = a + 1; b < limit; ++b )
+{
+for ( c = b + 1; c < limit; ++c )
+{
+if ( a * a + b * b == c * c )
+{
+printf("%3d : { %3d, %3d, %3d }\n", ++i, a, b, c);
+}
+}
+}
+}
+return 0;
+}
